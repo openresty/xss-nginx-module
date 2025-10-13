5c2993a20d1198795524feccb51b4be5682e49d3
lijunlong <lijunlong@openresty.com>
2025-10-13 19:01:06 +0800

    update openssl& pcre2

--- util/build.sh
+++ util/build.sh
@@ -15,24 +15,24 @@ version=$1
 force=$2
 
 ngx-build $force $version \
-            --with-cc-opt="-I$PCRE_INC -I$OPENSSL_INC" \
-            --with-ld-opt="-L$PCRE_LIB -L$OPENSSL_LIB -Wl,-rpath,$PCRE_LIB:$OPENSSL_LIB" \
-        --with-http_ssl_module \
-            --without-mail_pop3_module \
-            --without-mail_imap_module \
-            --without-mail_smtp_module \
-            --without-http_upstream_ip_hash_module \
-            --without-http_empty_gif_module \
-            --without-http_memcached_module \
-            --without-http_referer_module \
-            --without-http_autoindex_module \
-            --without-http_auth_basic_module \
-            --without-http_userid_module \
-          --add-module=../echo-nginx-module \
-          --add-module=../ndk-nginx-module \
-          --add-module=../lua-nginx-module \
-          --add-module=$root $opts \
-          --with-debug
+     --with-cc-opt="-I$PCRE_INC -I$OPENSSL_INC" \
+     --with-ld-opt="-L$PCRE_LIB -L$OPENSSL_LIB -Wl,-rpath,$PCRE_LIB:$OPENSSL_LIB" \
+     --with-http_ssl_module \
+     --without-mail_pop3_module \
+     --without-mail_imap_module \
+     --without-mail_smtp_module \
+     --without-http_upstream_ip_hash_module \
+     --without-http_empty_gif_module \
+     --without-http_memcached_module \
+     --without-http_referer_module \
+     --without-http_autoindex_module \
+     --without-http_auth_basic_module \
+     --without-http_userid_module \
+     --add-module=../echo-nginx-module \
+     --add-module=../ndk-nginx-module \
+     --add-module=../lua-nginx-module \
+     --add-module=$root $opts \
+     --with-debug
           #--with-cc-opt="-g3 -O0"
           #--add-module=$root/../echo-nginx-module \
   #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
